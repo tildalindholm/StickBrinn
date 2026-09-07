4712db7b89de1a5c061119baa9499b37a9269552
Amlambda <e.amanda.norberg@gmail.com>
2026-09-07 15:33:09 +0200

    m

--- Gemensam/rowCountBtn.js
+++ Gemensam/rowCountBtn.js
@@ -2,6 +2,7 @@ let ClickNextRow = document.getElementById("nextBtn");
 let ClickPrevRow = document.getElementById("prevBtn");
 let rowNumber = document
   .getElementsByClassName("row")[0]
+  // getElementsByClassName("row")[0] hämtar det första elementet med klassen "row" i dokumentet.
   .getElementsByTagName("span");
 let prevRowSpan = rowNumber[2];
 let currentRowSpan = rowNumber[1];
